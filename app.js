b6737b0c79b963acd79f7df98f90651986b4dc90
vamshikrishna061 <kandagatlavamshikrishna11>
2023-03-24 16:30:27 +0530

    TASK 2

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((request, response, next) => {
-    console.log('In the middleware!');
-    next();// Allowa the request to continue to the next middlw ware in line//
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(request, response, next) => {
+   response.send('<form action="/product" method="POST"><input type=""text" name="title"><button type="submit">Add product</button></form>')
 });
 
-app.use((request, response, next) => {
-    console.log('In another middleware!');
+app.use('/product', (request, response, next) =>{
+    console.log(request.body)
+    response.redirect('/')
+})
+
+app.use('/',(request, response, next) => {
     response.send('<h1> Hello for Express!</h1>')
 });
 
-
 app.listen(3000);
+
