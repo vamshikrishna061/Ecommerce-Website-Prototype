5c6ce7b1bb1ee4fd2a721c875ee66b991e8156ec
vamshikrishna061 <kandagatlavamshikrishna11>
2023-04-03 16:59:02 +0530

    Ts-7  Models

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,15 +3,18 @@ const Product = require('../models/product.js');
 const rootDir = require('../util/path.js');
 
 exports.getShop = (req,res,next)=>{
-    const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
+   Product.fetchAll((products=>{
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+
+   }));
+ 
     //const products = Product.fetchAll();
    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 }
