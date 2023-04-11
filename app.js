abb5b5e2954d7b97635ec3000bbe6fcecfd64f4e
vamshikrishna061 <kandagatlavamshikrishna11>
2023-04-11 17:38:10 +0530

    MYSQL

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-
-// const admin = require('./routes/admin.js');
-// const shop = require('./routes/shop.js');
-// const contactus = require('./routes/contactus.js');
-
-// const controller404 = require('./controllers/error');
-
-// const app = express();
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
-
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/admin',admin)
-
-// app.use('/shop',shop)
-
-// app.use('/contactus', contactus)
-
-// app.use('/',controller404.get404)
-
-// app.listen('3000');
-
-
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -40,6 +13,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
