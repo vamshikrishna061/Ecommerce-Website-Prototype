1b64f7fb078df59d4ae9d8d27188c6631b559aa1
vamshikrishna061 <kandagatlavamshikrishna11>
2023-07-11 15:27:00 +0530

    orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -71,22 +71,53 @@ exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
     .deleteItemFromCart(prodId)
-
     .then((result) => {
       res.redirect("/cart");
     })
     .catch((err) => console.log(err));
 };
 
-exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .addOrder()
+    // .populate("cart.items.productId")
+    // .then((user) => {
+    //   const products = user.cart.items.map((i) => {
+    //     return { quantity: i.quantity, product: { ...i.productId._doc } };
+    //   });
+    //   const order = new Order({
+    //     user: {
+    //       name: req.user.name,
+    //       userId: req.user,
+    //     },
+    //     products: products,
+    //   });
+    //   return order.save();
+    // })
+    //     .then((result) => {
+    //       return req.user.clearCart();
+    //     })
+    //     //.addOrder()
+    //     .then((result) => {
+    //       res.redirect("/orders");
+    //     })
+    //     .catch((err) => console.log(err));
+
+    .then((result) => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
 };
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
