155ea34cd58107c3590e68782a1d96e94c3a557b
vamshikrishna061 <kandagatlavamshikrishna11>
2023-07-24 14:48:46 +0530

    Added cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,6 +44,19 @@ exports.getIndex = (req, res, next) => {
     });
 };
 
+// exports.getCart = (req, res, next) => {
+//   req.user
+//     .getCart()
+//     .then((products) => {
+//       res.render("shop/cart", {
+//         path: "/cart",
+//         pageTitle: "Your Cart",
+//         products: products,
+//       });
+//     })
+
+//     .catch((err) => console.log(err));
+// };
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
@@ -54,9 +67,20 @@ exports.getCart = (req, res, next) => {
         products: products,
       });
     })
-
     .catch((err) => console.log(err));
 };
+// exports.postCart = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   Product.findById(prodId)
+//     .then((product) => {
+//       return req.user.addToCart(product);
+//     })
+//     .then((result) => {
+//       console.log(result);
+//       res.redirect("/cart");
+//     });
+// };
+
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
